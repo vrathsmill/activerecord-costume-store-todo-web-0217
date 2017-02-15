9c441ba924efce3b7aaecccee43ad0bcb20223fd
vrathsmill <vlrathsmill@gmail.com>
2017-02-15 17:49:01 -0500

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,7 @@
 # Create your CostumeStore class here
+
+class CostumeStore < ActiveRecord::Base
+end
+
+#
+# end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,17 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration
+  def change
+    create_table :haunted_houses do |t|
+      t.string  :name
+      t.text  :location
+      t.string :theme
+      t.integer    :price
+      t.boolean   :family_friendly
+      t.datetime    :closing_date
+      t.datetime    :opening_date
+      t.string      :description
+      t.timestamps
+    end
+  end
+end
